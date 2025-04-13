59017eab3cf4987541b05d3acbcad3bceee71f6e
devincar <devmcaron@gmail.com>
2025-04-13 09:48:08 -0700

    Added disputed message on facts with higher false votes than positive

--- src/App.js
+++ src/App.js
@@ -265,6 +265,8 @@ function FactList({ facts, setFacts }) {
 
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed =
+    fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
 
   async function handleVote(columnName) {
     setIsUpdating(true);
@@ -285,6 +287,7 @@ function Fact({ fact, setFacts }) {
   return (
     <li className="fact">
       <p>
+        {isDisputed ? <span className="disputed">[DISPUTED]</span> : null}
         {fact.text}
         <a className="source" href={fact.source} rel="noopener">
           (Source)
